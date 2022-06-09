93c2baac72acc00a5819991a1195375b543f738e
tyughiop324fcc <55851009+tyughiop324fcc@users.noreply.github.com>
2022-06-09 22:59:50 +0400

    app me

---
+++ app.js
@@ -0,0 +1,13 @@
+const firhtName = "Феодор";
+const age = 25;
+const a = 2;
+const b = 5;
+const lastName = prompt("Введите фамилию");
+alert(
+    firhtName + " " + lastName
+);
+alert(age);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
